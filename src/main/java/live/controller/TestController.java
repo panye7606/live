e087e51d9566dc0a8ec58960fa5159b2b319335c
zhuangshunhe <zhuangshunhe@zhiping.com>
2017-02-01 21:04:22 +0800

    搭建springmvc框架

---
+++ src/main/java/live/controller/TestController.java
@@ -0,0 +1,19 @@
+package live.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by rcer on 17/1/29.
+ */
+
+@Controller
+@RequestMapping("/welcome")
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
