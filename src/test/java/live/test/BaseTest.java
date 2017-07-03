796f9ab76c32c168f6c1f9bb535a7f691afd6485
radiocontroller <302643159@qq.com>
2017-07-03 19:10:33 +0800

    bug fix

--- src/test/java/live/test/BaseTest.java
+++ src/test/java/live/test/BaseTest.java
@@ -5,5 +5,5 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring/spring-*.xml" })
-public class BaseTest {
+public abstract class BaseTest {
 }
